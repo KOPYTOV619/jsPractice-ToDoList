cc370ddf2705065a8c1a3433672cc1ad24cf0f41
“Yury_Kopytov” <kopytov.yury@gmail.com>
2022-10-27 23:09:46 +0300

    Code completion / Доработка кода

--- js/script.js
+++ js/script.js
@@ -1,32 +1,57 @@
 'use strict';
 
-let taskBD = {
-	base: [
-		"Сходить в магазин",
-		"Выкинуть мусор и покормить двух котов",
-		"Полить цветы",
-		"Приготовить обед"
-	]
-};
+document.addEventListener('DOMContentLoaded', () => {
 
-let taskList = document.querySelector('.list__lines');
+	let taskBD = {
+		base: [
+			"Сходить в магазин",
+			"Выкинуть мусор и покормить двух котов",
+			"Полить цветы",
+			"Приготовить обед"
+		]
+	};
 
-//Очистим список
-taskList.innerHTML = "";
+	let taskList = document.querySelector('.list__lines'),
+		addForm = document.querySelector('form.add'),
+		addInput = addForm.querySelector('.add__input'),
+		checkbox = addForm.querySelector('[type="checkbox"]');
 
-//Сортировка списка задач в Базе по алфавиту
-taskBD.base.sort();
+	addForm.addEventListener('submit', (event) => {
+		event.preventDefault(); // Отключаем стандартное поведение браузера
+		let newLine = addInput.value; // Проверяем, что ввел пользователь
+		//let ready = checkbox.checked; // Проверяем нажат ли чекбокс
+		taskBD.base.push(newLine); // Добавляем новую задачу в БД
+		sortList(taskBD.base); // Сортировка списка задач в БД после добавления новой задачи 
+		createNewLine(taskBD.base, taskList); // Создаем новую задачу
+		event.target.reset(); // Очищаем форму после нажатия кнопки
+	});
 
-//Выводим на экран список задач из Базы
-taskBD.base.forEach((item, i) => {
-	taskList.innerHTML += `
+	// Сортировка списка задач в Базе по алфавиту
+	let sortList = (array) => {
+		array.sort();
+	};
+
+	sortList(taskBD.base);
+
+	// Создаем новую задачу
+	let createNewLine = (lists, parent) => {
+
+		parent.innerHTML = ""; // Очистим список
+
+		// Выводим на экран список задач из Базы
+		lists.forEach((item, i) => {
+			parent.innerHTML += `
 			<li class="list__lines-item">
 				<div class="list__lines-content">
-					<input class="checkbox" type="checkbox">
-					${i + 1}. ${item}
+					<input class="checkbox" type="checkbox">${i + 1}. ${item}
 				</div>
 				<div class="delete"></div>
 			</li>
 			<span class="list__lines-span"></span>
 		`;
+		});
+	};
+
+	createNewLine(taskBD.base, taskList);
+
 });
