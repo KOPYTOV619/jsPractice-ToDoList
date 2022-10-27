084f9e2801baf195ae4b09cdbd7f7dadd294cb6e
“Yury_Kopytov” <kopytov.yury@gmail.com>
2022-10-27 18:33:31 +0300

    Added basic design / Добавил базовый дизайн

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 let taskBD = {
 	base: [
 		"Сходить в магазин",
-		"Выкинуть мусор",
+		"Выкинуть мусор и покормить двух котов",
 		"Полить цветы",
 		"Приготовить обед"
 	]
@@ -12,7 +12,7 @@ let taskBD = {
 let taskList = document.querySelector('.list__lines');
 
 //Очистим список
-	taskList.innerHTML = "";
+taskList.innerHTML = "";
 
 //Сортировка списка задач в Базе по алфавиту
 taskBD.base.sort();
@@ -20,8 +20,13 @@ taskBD.base.sort();
 //Выводим на экран список задач из Базы
 taskBD.base.forEach((item, i) => {
 	taskList.innerHTML += `
-			<li class="line"> <input type="checkbox"> ${i + 1}. ${item}
-				<div class="delete"><img src="/icons/cross-with-circle.svg" alt="img"class="icon_delete"></div>
+			<li class="list__lines-item">
+				<div class="list__lines-content">
+					<input class="checkbox" type="checkbox">
+					${i + 1}. ${item}
+				</div>
+				<div class="delete"></div>
 			</li>
+			<span class="list__lines-span"></span>
 		`;
 });
